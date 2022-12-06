32b4d9c2623fce7e0fa2204ab6ad50e3b5bc5b6e
devilbats011 <wanafnanharizwz@gmail.com>
2022-12-06 18:51:28 +0800

    update-

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-import './style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function main() {
+  const futsalCourtEl = document.querySelector(".futsal-court");
+
+  for (let index = 4; index > 0; index--) {
+    //Clone Courts
+    const futsalCourtElClone = futsalCourtEl.cloneNode(true);
+    // let futsalCourtTextEL = futsalCourtElClone;
+    const futsalCourtTextEL =
+    futsalCourtElClone.getElementsByClassName("card-title")[0];
+    futsalCourtTextEL.innerText = "COURT " + (index + 1);
+    // futsalCourtTextEL.tex
+    console.log(futsalCourtTextEL);
+    // Inject after the element
+    futsalCourtEl.after(futsalCourtElClone);
+  }
+
+}
+main();
